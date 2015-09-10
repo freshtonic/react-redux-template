5323c5da562dfdb39f25f4b29d6bbaa19aaee2d3
James Sadler <freshtonic@gmail.com>
2015-09-10 17:57:33 +1000

    BUG: Adding a <Link> causes client side infinite redirect
    To reproduce, run the app with this change and watch the URL bar. In Chrome you
    won't be able to close the browser tab. If you delete the <Link> line, the hot
    code reload feature will kick in and replace the code and you'll be able to
    close your browser tab.

--- app/components/home-page/index.jsx
+++ app/components/home-page/index.jsx
@@ -1,11 +1,13 @@
 import 'home-page/home-page.css'
 import React from 'react'
 import SimpleComponent from 'simple-component'
+import Link from 'react-router'
 
 export default class HomePage extends React.Component {
 	render() {
 		return (
 			<section className='home-page'>
+				<Link to="home">HOME</Link>
 				<SimpleComponent color='red' />
 			</section>
 		)
